c718df99e3ebcf7589f7f1722acf126a4e7fa804
Mozhavro <mozhario@gmail.com>
2017-12-11 16:05:38 +0200

    added config and env file

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv(), override=True)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{path}/test.db'.format(
+                            path=BASE_DIR)
+
+WEATHER_API_SECRET_KEY = os.environ.get('WEATHER_API_SECRET_KEY')
+
