3024741858add3cb4721fb24b4ecbe854bb0a096
Mozhavro <mozhario@gmail.com>
2017-12-11 15:55:33 +0200

    added models layer

---
+++ models.py
@@ -0,0 +1,17 @@
+from .app import db
+
+
+class WeatherSnapshot(db.Model):
+    """
+    Represent a snapshot of a weather stats for a specific time
+    and place.
+    """
+    __tablename__ = 'weather_entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    temperature = db.Column(db.Integer())
+    place = db.Column(db.String())
+    date = db.Column(db.DateTime())
+
+    def __str__(self):
+        return '<WeatherSnapshot id:{id}>'.format(id=self.id)
