e1fe7c863c3249727162158ad20334548b7411b4
Mozhavro <mozhario@gmail.com>
2017-12-12 13:31:40 +0200

    added user model

--- models.py
+++ models.py
@@ -1,10 +1,35 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class WeatherSnapshot(db.Model):
     """
     Represent a snapshot of a weather stats for a specific time
     and place.
+
+    Used to cache API calls results.
     """
     __tablename__ = 'weather_snapshots'
 
