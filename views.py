52d4cea3af4fd8c534cd5f23508f813ab76cb38f
Mozhavro <mozhario@gmail.com>
2017-12-13 15:16:37 +0200

    cleanup

--- views.py
+++ views.py
@@ -4,7 +4,6 @@ from flask_login import login_user, logout_user, login_required
 
 # TODO use current_app
 from app import app, db
-# from models import User
 import models
 import forms
 from services import WeatherService
@@ -32,16 +31,13 @@ def get_temperature_between_dates():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = forms.LoginForm()
-    valid =form.validate_on_submit()
     if form.validate_on_submit():
         user = models.User.query.filter_by(username=form.username.data).first()
-        all_users = models.User.query.order_by(models.User.username).all()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or "/")
 
         flash('Invalid username or password.')
-    # import pudb; pudb.set_trace()
 
     return render_template('auth/login.html', form=form)
 
